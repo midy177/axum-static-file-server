e0ba69eb63d071b72f70300c340c0f460d44d204
Your Name <your@example.com>
2024-05-30 16:22:09 +0800

    修改build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #/bin/bash
-docker run -it --rm rust:1.68.0 bash
-cargo install --path .
-cargo build --release
+docker buildx build --platform linux/amd64,linux/arm64/v8 --rm --push \
+-t dockerhub.milesight.cn/milesight/axum-static-file-server:v1.0.2 -f Dockerfile .
+#docker run -it --rm rust:1.68.0 bash
+#cargo install --path .
+#cargo build --release
